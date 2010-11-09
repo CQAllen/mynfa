0664a86a8a2bc5ee283dd9e687442c4164c926e0
chengy1013 <chengy1013@gmail.com>
2010-11-09 01:28:23 +0000

    DFA的最小化

---
+++ NFA/src/compiler/nfa/DFAMinimize.java
@@ -0,0 +1,23 @@
+package compiler.nfa;
+
+import java.util.List;
+
+import compiler.DFA.entity.DFA_Map;
+
+/**
+ * DFA最小化
+ * 
+ * @author Cheng
+ * 
+ */
+public class DFAMinimize {
+
+	private List<DFA_Map> DFA_Map;
+
+	/**
+	 * 构造函数，需要通过NFA转化来的DFA
+	 */
+	public DFAMinimize(List<DFA_Map> DFA_Map) {
+		this.DFA_Map = DFA_Map;
+	}
+}
